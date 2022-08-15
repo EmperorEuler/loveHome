09a35cb21fc742fc39e7fdac493e294ce7487ecc
EmperorEuler <[chengzhao.chen2992@gmail.com](mailto:chengzhao.chen2992@gmail.com)>
2022-08-15 01:52:36 -0700

    配置redis环境和fdfs开发环境和上传头像功能

---
+++ models/fdfs.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"loveHome/fdfs_client"
+
+	"github.com/astaxie/beego"
+)
+
+func TestUploadByFilename(fileName string) (groupName string, FileId string, err error) {
+	fdfsClient, errClient := fdfs_client.NewFdfsClient("../conf/client.conf")
+	if err != nil {
+		beego.Info("Newo FdfsClient error  %s", errClient.Error())
+		return "", "", errClient
+	}
+	uploadResponse, errUpload := fdfsClient.UploadByFilename("fileName")
+	if errUpload != nil {
+		beego.Info("New FdfsClient err %s", errUpload.Error())
+		return "", "", errUpload
+	}
+	return uploadResponse.GroupName, uploadResponse.RemoteFileId, nil
+}
